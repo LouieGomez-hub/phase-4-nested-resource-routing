507f8204a976af3547b139f0a0b51da14e0d9357
louie_gomez <kinggomez027.lg@gmail.com>
2023-03-13 14:40:14 -0400

    nested resource routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
-  resources :dog_houses, only: [:show]
-  resources :reviews, only: [:show, :index, :create]
 
-  get '/dog_houses/:dog_house_id/reviews', to: 'dog_houses#reviews_index'
-  get '/dog_houses/:dog_house_id/reviews/:id', to: 'dog_houses#review'
+  resources :dog_houses, only: [:show] do
+    # nested resource for reviews
+    resources :reviews, only: [:show, :index]
+  end
+
+  resources :reviews, only: [:show, :index, :create]
 end
